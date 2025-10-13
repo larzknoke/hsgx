c142a292ebe54196f9c732f26ef331f4a2b3169d
Lars Knoke <larz.knoke@gmail.com>
2025-10-13 07:05:40 +0200

    init

---
+++ src/app/admin/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Admin() {
+  return (
+    <div>
+      Admin
+      <Button variant="outline">Click Me</Button>
+    </div>
+  );
+}
+
+export default Admin;
