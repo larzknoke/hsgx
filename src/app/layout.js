081aabfcd542637eb7e36627976848a5592e94f1
Lars Knoke <larz.knoke@gmail.com>
2025-10-26 22:05:11 +0100

    swap

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,13 @@ import { Sigma } from "lucide-react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata = {
