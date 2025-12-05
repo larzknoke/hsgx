5e8f02aafbce694603d3ce0075b7ef2cbb91d306
Lars Knoke <larz.knoke@gmail.com>
2025-12-05 23:51:47 +0100

    trainerTeams

--- src/app/team/components/teamTable.jsx
+++ src/app/team/components/teamTable.jsx
@@ -93,9 +93,7 @@ function TeamTable({ teams, trainers }) {
               <TableCell className="font-medium">{team.id}</TableCell>
               <TableCell className="font-medium">{team.name}</TableCell>
               <TableCell>
-                {team.TrainerTeamRelation.map((tt) => tt.Trainer.name).join(
-                  ", "
-                )}
+                {team.trainerTeams.map((tt) => tt.trainer.name).join(", ")}
               </TableCell>
               <TableCell>{team.playerTeams?.length || 0}</TableCell>
               <TableCell className="text-right">
